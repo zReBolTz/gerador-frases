cad67e11567d296a7951c27507f9016ed25c0625
zReBolTz <ferreiraw36@gmail.com>
2025-01-15 20:50:02 -0300

    criando funções

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,64 @@ import logo from "./assets/logo.png";
 import "./App.css";
 
 function App() {
+  const [textoFrase, setTextoFrase] = useState("");
+  const [category, setCategory] = useState(0);
+  const AllFrases = [
+    {
+      id: 1,
+      nome: "Motivação",
+      frases: [
+        "Siga os bons e aprenda com eles.",
+        "O bom-senso vale mais do que muito conhecimento.",
+        "O riso é a menor distância entre duas pessoas.",
+        "Deixe de lado as preocupações e seja feliz.",
+        "Realize o óbvio, pense no improvável e conquiste o impossível.",
+        "Acredite em milagres, mas não dependa deles.",
+        "A maior barreira para o sucesso é o medo do fracasso.",
+      ],
+    },
+    {
+      id: 2,
+      nome: "Bom dia",
+      frases: [
+        "Acordar de bem com a vida é o primeiro passo para ter um dia abençoado! Bom dia, família!",
+        "A melhor forma de acordar é pular da cama e se preparar para correr atrás de todos os seus sonhos! Bom dia, mundo!",
+        "Escreva em seu coração: todo dia é o melhor dia do ano.",
+        "Bom dia! Não se esqueça que a sua alma é o reflexo do sol, tão forte e brilhante quanto um girassol.",
+      ],
+    },
+  ];
+  function gerarFrase() {
+    let fraseAleatoria = Math.floor(
+      Math.random() * AllFrases[category].frases.length
+    );
+    setTextoFrase(`" ${AllFrases[category].frases[fraseAleatoria]} "`);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="Logo Frases" className="logo" />
 
       <h1 className="title">Categorias</h1>
       <section className="category-area">
-        <button className="category-bottom">Motivações</button>
-        <button className="category-bottom">Bom dia</button>
+        {AllFrases.map((item, index) => (
+          <button
+            key={item.id}
+            className="category-button"
+            style={{
+              borderWidth: item.nome === AllFrases[category].nome ? 3 : 0,
+              borderColor: "#1fa4db",
+            }}
+            onClick={() => setCategory(index)}
+          >
+            {item.nome}
+          </button>
+        ))}
       </section>
-      <button className="button-frase">Gerar Frase</button>
-      <p className="texto-frase">A frase está aqui</p>
+      <button className="button-frase" onClick={gerarFrase}>
+        Gerar Frase
+      </button>
+      {textoFrase !== "" && <p className="texto-frase">{textoFrase}</p>}
     </div>
   );
 }
